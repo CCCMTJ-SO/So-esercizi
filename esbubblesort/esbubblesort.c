d5266e6e764a96c30d757c503ac3bb3e42460b4f
cecco <cecco@cccmitjaLaptop>
2020-04-04 18:18:42 +0200

    aggiunta di insertion sort + metodo swap

--- esbubblesort/esbubblesort.c
+++ esbubblesort/esbubblesort.c
@@ -1,49 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void bubble_sort(int* array,unsigned int dim)
-{
-if (dim == 1) return;
-	for(int i = 0; i < dim-1; i++)
+void swap(int array[], unsigned int x, unsigned int y)
 	{
-	if ( array[i] > array[i+1])
+	int temp = array[x];
+	array[x] = array[y];
+	array[y] = temp;
+	}
+
+void bubble_sort(int array[],unsigned int dim)
+	{
+	if (dim == 1) return;
+		for(int i = 0; i < dim-1; i++)
 		{
-		 int temp = array[i];
-         array[i] = array[i+1];
-	     array[i+1] = temp;
+			if ( array[i] > array[i+1])
+			{
+			swap(array,i,i+1);
+			}
 		}
-	}
-if (dim >= 1){bubble_sort(array,dim-1);}
-
-}
-
-void stampa_array(int* array,unsigned int dim)
-{
-	printf("Indice: ");
-	for(int i = 0; i < dim;i++)
-	{
-
-	printf("%d",i);
-    int c = array[i];
-    do {printf(" "); c = c/10;}
-    while (c >=  1);
+	if (dim >= 1){bubble_sort(array,dim-1);}
 
 	}
-	printf("\n");
-	printf("Valori: ");
-	for(int i = 0; i < dim;i++)
+
+void stampa_array(int array[],unsigned int dim)
 	{
-
-	printf("%d ",array[i]);
-
-		}
+		printf("Indice: ");
+		for(int i = 0; i < dim;i++)
+			{
+			printf("%d",i);
+			int c = array[i];
+			do {printf(" "); c = c/10;}
+			while (c >=  1);
+			}
+		printf("\n");
+		printf("Valori: ");
+		for(int i = 0; i < dim;i++)
+			{
+			printf("%d ",array[i]);
+			}
 	printf("\n\n");
 
-}
+	}
+
+void insertion_sort(int array[],unsigned int dim)
+	{
+	int j = 0;
+		for(int i = 1; i < dim;i++)
+		{
+			while( j < i)
+			{
+			if (array[j] > array[i]) swap(array,j,i);
+			j++;
+			}
+		j = 0;
+		}
 
 
+
+	}
 int main()
-{
+	{
 
 	printf("Quanto è lungo l'array?\n");
 
@@ -70,9 +87,18 @@ int main()
 			printf("Ecco l'array: \n");
 			stampa_array(array,dim);
 
-			printf("Ecco l'array ordinato: \n");
+			int array2[dim];
+			memcpy(array2,array,dim*sizeof(int));
+
+			printf("Ecco l'array ordinato con bubble sort: \n");
 			bubble_sort(array,dim);
 			stampa_array(array,dim);
 
-return 0;
-}
+			printf("Ecco l'array ordinato con insertion sort: \n");
+			insertion_sort(array2,dim);
+			stampa_array(array2,dim);
+
+
+
+	return 0;
+	}
